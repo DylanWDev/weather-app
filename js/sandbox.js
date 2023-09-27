4cc33c938bbfcf44478c1d61ec64f2cd7677dd8d
Dylan Wyrick <xpoweroutage@gmail.com>
2023-09-27 17:38:37 -0400

    added working input field

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,27 +1,43 @@
+let state = 0
 const apiKey = "9a522097e4ef3c0ac50fa6944a8b3d33"
-const zipCode = "40513"
-const api_url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}`;
-function init() {
-    console.log('hello sandbox');
-    test_axios()
-}
+let zipCode = "20314"
+let temp = ""
+let condition = ""
+
+
+
+console.log("this runs");
 
 async function test_axios(){
+    const api_url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}`;
     const result = await axios.get(api_url);
-    console.log(result);
+    console.log(result.data.name);
+
 }
 
  // Create a new div element
  let newDiv = document.createElement("div");
  document.body.appendChild(newDiv);
+ 
+ let newButton = document.createElement("button");
+ newButton.innerHTML = "get zip";
+ newDiv.appendChild(newButton);
+ newButton.id = "testBtn"
 
-let newButton = document.createElement("button");
-newButton.innerHTML = "hi";
-newDiv.appendChild(newButton);
 
-newButton.id = "testBtn"
+let newP = document.createElement("input")
+newDiv.appendChild(newP)
+newP.id = "inputZip"
 
-// document.getElementById("testBtn").addEventListener('click', )
+
+document.getElementById("testBtn").addEventListener('click', getZip)
+
+function getZip(){
+    let inputField = document.getElementById("inputZip")
+    zipCode = inputField.value 
+    // console.log(inputField.value)
+    test_axios();
+}
 
 
 
