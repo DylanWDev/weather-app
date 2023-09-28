f41a8c5aa5fd2d7abbeba0afe5dd254a10c9f3e7
Dylan Wyrick <xpoweroutage@gmail.com>
2023-09-28 18:39:43 -0400

    got row to work

--- js/sandbox.js
+++ js/sandbox.js
@@ -1,6 +1,6 @@
 //* global variables
 let state = 0
-let zipCode = "20314"
+let zipCode = ""
 let temp = ""
 let condition = ""
 const apiKey = "9a522097e4ef3c0ac50fa6944a8b3d33"
@@ -8,20 +8,7 @@ const apiKey = "9a522097e4ef3c0ac50fa6944a8b3d33"
 
 
 //* runs the api
-async function test_axios() {
-    try {
-        const api_url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}`;
-        const result = await axios.get(api_url);
-        console.log(result.data.main.temp)
-        console.log(result.data.name);
-        if (result) {
-            fillCity(result.data.name);
-        }
-    } catch (error) {
-        alert("zip code not found", error);
-        // You can display an error message to the user here if needed
-    }
-}
+
 //* runs the api
 
 
@@ -60,12 +47,16 @@ topDiv.classList = "d-flex justify-content-center"
  topDiv.appendChild(newButton);
  newButton.id = "testBtn"
 
-document.getElementById("testBtn").addEventListener('click', getZip)
+document.getElementById("testBtn").addEventListener('click', function(){
+    getWeather()
+})
 
-function getZip(){
+async function getWeather(){
     let inputField = document.getElementById("input")
     zipCode = inputField.value 
     test_axios().then(console.log('got city'));
+
+    
 }
 //* input and button area
 
@@ -86,22 +77,24 @@ infoDiv.appendChild(cityDiv)
 cityDiv.id = "cityDiv"
 cityDiv.classList = "d-block border border-dark border-3"
 
-let city = document.createElement("p")
+let city = document.createElement("h6")
 city.innerText = "City"
 cityDiv.appendChild(city)
 city.id = "city"
 city.classList = "d-flex justify-content-center mb-0 border-bottom border-dark bg-info"
 
+
 let cityName = document.createElement("p")
 cityDiv.appendChild(cityName)
 cityName.id = "cityName"
 cityName.classList = "d-flex justify-content-center mb-0 align-items-center"
-//* city area
-
 
 function fillCity(city){
     cityName.innerText = city
 }
+//* city area
+
+
 
 
 //* temperature area
@@ -110,13 +103,58 @@ infoDiv.appendChild(tempDiv);
 tempDiv.id = "tempDiv";
 tempDiv.classList = "d-block border border-dark border-3 mt-3";
 
-let tempMain = document.createElement("p");
+let tempMain = document.createElement("h5");
 tempMain.innerHTML = "temperature";
 tempDiv.appendChild(tempMain);
+tempMain.id = "tempMain"
+tempMain.classList = "d-flex justify-content-center mb-0 border-dark bg-info"
 
-let tempName = document.createElement("p");
-tempDiv.appendChild(tempName);
+//*row container
+let tempContainDiv = document.createElement("div");
+infoDiv.appendChild(tempContainDiv);
+tempContainDiv.id = "TempContainDiv";
+tempContainDiv.classList = "d-flex justify-content-between border border-dark border-3 border-top-0"; // Use justify-content-between for a row layout
+//*row container
+
+let tempK = document.createElement("div");
+tempContainDiv.appendChild(tempK);
+tempK.id = "tempK";
+tempK.innerHTML = "hi";
+tempK.classList = "col-4 d-flex justify-content-center mb-0 align-items-center";
+
+let tempF = document.createElement("div");
+tempContainDiv.appendChild(tempF);
+tempF.id = "tempF";
+tempF.innerHTML = "hi";
+tempF.classList = "col-4 d-flex justify-content-center mb-0 align-items-center";
+
+let tempC = document.createElement("div");
+tempContainDiv.appendChild(tempC);
+tempC.id = "tempC";
+tempC.innerHTML = "hi";
+tempC.classList = "col-4 d-flex justify-content-center mb-0 align-items-center";
+
+
 //* temperature area
+
+async function test_axios() {
+    try {
+        const api_url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}`;
+        const result = await axios.get(api_url);
+        console.log(result.data.main.temp)
+        console.log(result.data.name);
+        if (result) {
+            fillCity(result.data.name);
+        }
+
+        function fillTemp(temp){
+            tempK = result
+        }
+    } catch (error) {
+        alert("zip code not found", error);
+        // You can display an error message to the user here if needed
+    }
+}
 
 
 
