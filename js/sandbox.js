704169f20ba0d884b2576edb7aaefaf340e9c5ec
Dylan Wyrick <xpoweroutage@gmail.com>
2023-09-28 20:35:01 -0400

    everything but image on condition

--- js/sandbox.js
+++ js/sandbox.js
@@ -54,7 +54,7 @@ document.getElementById("testBtn").addEventListener('click', function(){
 async function getWeather(){
     let inputField = document.getElementById("input")
     zipCode = inputField.value 
-    test_axios().then(console.log('got city'));
+    init().then(console.log('got city'));
 
     
 }
@@ -88,10 +88,6 @@ let cityName = document.createElement("p")
 cityDiv.appendChild(cityName)
 cityName.id = "cityName"
 cityName.classList = "d-flex justify-content-center mb-0 align-items-center"
-
-function fillCity(city){
-    cityName.innerText = city
-}
 //* city area
 
 
@@ -103,41 +99,84 @@ infoDiv.appendChild(tempDiv);
 tempDiv.id = "tempDiv";
 tempDiv.classList = "d-block border border-dark border-3 mt-3";
 
-let tempMain = document.createElement("h5");
-tempMain.innerHTML = "temperature";
+let tempMain = document.createElement("h6");
+tempMain.innerHTML = "Temperature";
 tempDiv.appendChild(tempMain);
 tempMain.id = "tempMain"
-tempMain.classList = "d-flex justify-content-center mb-0 border-dark bg-info"
+tempMain.classList = "d-flex justify-content-center border border-0 mb-0 border-dark bg-info"
 
 //*row container
 let tempContainDiv = document.createElement("div");
 infoDiv.appendChild(tempContainDiv);
 tempContainDiv.id = "TempContainDiv";
-tempContainDiv.classList = "d-flex justify-content-between border border-dark border-3 border-top-0"; // Use justify-content-between for a row layout
+tempContainDiv.classList = "mb-3 d-flex justify-content-between border border-dark border-3 border-top-0";
 //*row container
+
 
 let tempK = document.createElement("div");
 tempContainDiv.appendChild(tempK);
 tempK.id = "tempK";
-tempK.innerHTML = "hi";
+tempK.innerHTML = "";
 tempK.classList = "col-4 d-flex justify-content-center mb-0 align-items-center";
 
 let tempF = document.createElement("div");
 tempContainDiv.appendChild(tempF);
 tempF.id = "tempF";
-tempF.innerHTML = "hi";
-tempF.classList = "col-4 d-flex justify-content-center mb-0 align-items-center";
+tempF.innerHTML = "";
+tempF.classList = "col-4 d-flex justify-content-center mb-0 align-items-center border border-black border-3 border-top-0 border-bottom-0 border-left border-right";
 
 let tempC = document.createElement("div");
 tempContainDiv.appendChild(tempC);
 tempC.id = "tempC";
-tempC.innerHTML = "hi";
+tempC.innerHTML = "";
 tempC.classList = "col-4 d-flex justify-content-center mb-0 align-items-center";
-
-
 //* temperature area
 
-async function test_axios() {
+
+//* condition
+let conditionDiv = document.createElement("div")
+infoDiv.appendChild(conditionDiv)
+conditionDiv.id = "conditionDiv"
+conditionDiv.classList = "d-block border border-dark border-3"
+
+let conditionName = document.createElement("h6")
+conditionName.innerText = "Condition"
+conditionDiv.appendChild(conditionName)
+conditionName.id = "conditionName"
+conditionName.classList = "d-flex justify-content-center mb-0 border-bottom border-dark bg-info"
+
+
+let conditionFill = document.createElement("p")
+conditionDiv.appendChild(conditionFill)
+conditionFill.id = "conditionFill"
+conditionFill.classList = "d-flex justify-content-center mb-0 align-items-center"
+//* condition
+
+
+
+//* image
+let otherDiv = document.createElement("div");
+otherDiv.id = "otherDiv";
+otherDiv.classList = "mt-3 d-block border border-dark border-3";
+infoDiv.appendChild(otherDiv);
+
+let otherName = document.createElement("h6");
+otherName.innerText = "otherName";
+otherName.id = "otherName"; // Use a unique ID
+otherName.classList = "d-flex justify-content-center mb-0 border-bottom border-dark bg-info";
+otherDiv.appendChild(otherName);
+
+let otherFill = document.createElement("p");
+otherFill.id = "otherFill";
+otherFill.classList = "d-flex justify-content-center mb-0 align-items-center";
+otherDiv.appendChild(otherFill);
+//* image
+
+
+
+
+//* main function to run api data
+async function init() {
     try {
         const api_url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}`;
         const result = await axios.get(api_url);
@@ -147,9 +186,28 @@ async function test_axios() {
             fillCity(result.data.name);
         }
 
-        function fillTemp(temp){
-            tempK = result
+        //* function to populate the city
+        function fillCity(city){
+            cityName.innerText = city
         }
+
+        //* function to populate the temp
+        function fillTemp(){
+            console.log(Math.round(result.data.main.temp) )
+            tempK.textContent = Math.round(result.data.main.temp) + "K"
+            tempF.textContent = Math.round((result.data.main.temp - 273.15) * 9/5 +32) + "F"
+            tempC.textContent = Math.round(result.data.main.temp - 273.15) + "C"
+        }
+        fillTemp()
+
+        function fillCondition(){
+            console.log(result.data.weather[0].main)
+            conditionFill.textContent = result.data.weather[0].main
+        }
+        fillCondition()
+        
+        
+        
     } catch (error) {
         alert("zip code not found", error);
         // You can display an error message to the user here if needed
